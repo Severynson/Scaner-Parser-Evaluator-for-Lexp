9a24db52634e22fb72736df3b1a7dbcaec335bd7
Severyn <severynson.kurach@gmail.com>
2025-03-09 23:06:42 -0700

    Missing ')' error handled successful

--- parser.py
+++ parser.py
@@ -1,6 +1,11 @@
-import os
-import argparse
+# Student: Severyn Kurach
+# Project Phase: 2.1
+
+import os, sys, argparse
 from scaner import scan_file_to_tokenized_lines, TokenType
+
+line_currently_parsed, line_num_currently_parsed = None, None
+output_file = "test_output.txt"
 
 
 initNode = lambda nodeValue, children, nodeType="non-terminal": {
@@ -54,10 +59,10 @@ def parse_element(tokens, position):
     """Parse an element: ( expression ) | NUMBER | IDENTIFIER"""
     if tokens[position]["token"] == "(":
         node, position = parse_expression(tokens, position + 1)
-        if tokens[position]["token"] == ")":
+        if position >= len(tokens):
+            error("Unexpected end of input: missing closing parenthesis")
+        elif tokens[position]["token"] == ")":
             return node, position + 1
-        else:
-            raise SyntaxError(f"Expected ')', found {tokens[position]['token']}")
     elif tokens[position]["tokenType"] == TokenType.NUM.value:
         node = initNode(tokens[position], [], "terminal")
         return node, position + 1
@@ -65,7 +70,7 @@ def parse_element(tokens, position):
         node = initNode(tokens[position], [], "terminal")
         return node, position + 1
     else:
-        raise SyntaxError(f"Unexpected token: {tokens[position]}")
+        error(f"Unexpected token: {tokens[position]}")
 
 
 def write_ast(node, file_to_write, indentation=0):
@@ -83,24 +88,41 @@ def write_ast(node, file_to_write, indentation=0):
 
 
 def test_driver(input_file, output_file="test_output.txt"):
+    global line_currently_parsed
+    global line_num_currently_parsed
     tokenized_lines = scan_file_to_tokenized_lines(input_file)
     with open(output_file, "w") as file_to_write:
         for tokenized_line in tokenized_lines:
+            line_currently_parsed = tokenized_line["line"]
+            line_num_currently_parsed = tokenized_line["line_number"]
             ast, _ = parse_expression(tokenized_line["tokens"], 0)
             file_to_write.write(
-                f'Line {tokenized_line["line_number"]} "{tokenized_line["line"]}":\n'
+                f'Line {line_currently_parsed} "{line_num_currently_parsed}":\n'
             )
             write_ast(ast, file_to_write)
 
 
+def error(message):
+    with open(output_file, "w") as f:
+        f.write(f"Parsing Error: {message}\n")
+        f.write(
+            f'In the line #{line_num_currently_parsed}: "{line_currently_parsed}"\n'
+        )
+    sys.exit(1)
+
+
 def main():
+    global output_file
     parser = argparse.ArgumentParser(description="parse based on the type of arguments")
     parser.add_argument("arguments", nargs="+", help="Runtime arguments")
     args = parser.parse_args()
     if len(args.arguments) in [1, 2] and os.path.exists(args.arguments[0]):
+        input_file = args.arguments[0]
+        if len(args.arguments) == 2:
+            output_file = args.arguments[1]
         test_driver(
-            args.arguments[0],
-            args.arguments[1] if len(args.arguments) == 2 else "test_output.txt",
+            input_file,
+            output_file,
         )
     elif not os.path.exists(args.arguments[0]):
         if not "." in args.arguments[0]:
